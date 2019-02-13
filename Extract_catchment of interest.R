30e97c06726b6cf2458b837662ceec0708baa9cc
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2019-02-13 11:01:34 +1000

    fit with upstream.mobility

--- Extract_catchment of interest.R
+++ Extract_catchment of interest.R
@@ -1,8 +1,11 @@
 library(catchstats)
 library(maptools)
 
-shapfile<-readShapePoly("H:/My Drive/PhD at GU/Part 2 Daily stream flow simulation/Gauges in Tamar/spatial/AFGH_Catch_Tamar_clean.shp")
-pour.seg<-c("5799")
+#shapfile<-readShapePoly("H:/My Drive/PhD at GU/Part 2 Daily stream flow simulation/Gauges in Tamar/spatial/AFGH_Catch_Tamar_clean.shp")
+shapfile<-SEQ.Clip
+pour.seg<-SegNo
+
+extract.ups(shapfile = shapfile,pour.seg =SegNo)
 
 extract.ups<-function(shapfile,pour.seg){
   hierarchy_1<-data.frame(site=shapfile$SegmentNo,nextds=shapfile$DWNID1)
@@ -10,4 +13,9 @@ extract.ups<-function(shapfile,pour.seg){
   for(i in 1:length(lst_1)){
     cat("\"SegmentNo\"","=",lst_1[i],"OR",file = paste0("Ups-",pour.seg,".txt"),append = TRUE)
   }
+}
+
+lst_1<-upstream.mobility
+for(i in 1:length(lst_1)){
+  cat("\"SegmentNo\"","=",lst_1[i],"OR",file = paste0("Ups-",pour.seg,".txt"),append = TRUE)
 }
