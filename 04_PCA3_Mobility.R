fa92e885276fecb21c55a478b14e255e58c7734d
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2019-08-14 19:14:54 +1000

    simulated annealling algorithm

--- 04_PCA3_Mobility.R
+++ 04_PCA3_Mobility.R
@@ -26,7 +26,7 @@ low.mobility<-5
 medium.mobility<-10
 high.mobility<-15
 
-PCA.water.only<-readShapeLines("Data/Shapfile/Threshold of quant 0.5/PCA_Water only_sw08_fp05")
+PCA.water.only<-readShapeLines("Data/Shapfile/Threshold of quant 0.5/PCA_Water only")
 names(PCA.water.only)
 
 dam.segment<-c(859803,853302,856476,874709)  # consider the blocking effect of dams
@@ -37,7 +37,7 @@ inundt.SegNo<-innudt.shp$SegmentNo
 delete.seg<-c(859398,859529,856156)
 inundt.SegNo<-inundt.SegNo[-match(delete.seg,inundt.SegNo)]
 
-PCA1_SegNo<-PCA.water.only$SegmentNo[PCA.water.only$Freq==1]
+PCA1_SegNo<-PCA.water.only$SegmentNo[PCA.water.only$Freq_15==1]
 reachable.PCA.L<-all_reachable_PCAs(PCA=PCA1_SegNo,mobility = low.mobility)
 reachable.PCA.M<-all_reachable_PCAs(PCA=PCA1_SegNo,mobility = medium.mobility)
 reachable.PCA.H<-all_reachable_PCAs(PCA=PCA1_SegNo,mobility = high.mobility)
@@ -48,23 +48,23 @@ PCA1_SP_SegNo<-intersect(PCA1_SegNo,species.distribution.df$SegNo)
 PCA1_SP_SegNo<-setdiff(PCA1_SP_SegNo,inundt.SegNo)  # also exclude inundated SegNo
 
 # calculate # of refugia (PCA1) each species has (i.e. species distribution)
-PCA1_SP_distribution<-species.distribution.df[match(PCA1_SP_SegNo,species.distribution.df$SegNo),]
-n.PCA1.SP<-colSums(PCA1_SP_distribution[,-1])
+#PCA1_SP_distribution<-species.distribution.df[match(PCA1_SP_SegNo,species.distribution.df$SegNo),]
+#n.PCA1.SP<-colSums(PCA1_SP_distribution[,-1])
 n.sp<-colSums(species.distribution.df[,-1])
 
-scaling.factor<-c(0.25)  # used to set conservation target (% of species distribution).
-cons.target<-floor(n.PCA1.SP*scaling.factor)
+scaling.factor<-c(0.15)  # used to set conservation target (% of species distribution).
+#cons.target<-floor(n.PCA1.SP*scaling.factor)
 cons.target<-floor(n.sp*scaling.factor)
 
 para.a<-0.4   # position penalty weight
-para.b<-1   # feature penalty weight
+para.b<-1  # feature penalty weight
 rep.sp<-c()
 n.seg<-c()
 solution.lst<-list()
 obj.func<-c()
 zero.remaining.pool<-0
 
-for(i in 1:1000){
+for(i in 1:2){
   
   remaining.seg<-PCA1_SP_SegNo
   #------
@@ -91,17 +91,17 @@ for(i in 1:1000){
   #------
   
   # 1) reachable segments from the retained
-  species.selected<-colnames(species.distribution.df[,-1])[species.distribution.df[match(remaining.seg[n],species.distribution.df$SegNo),-1]==1]
-  least.mobility<-min(sp.mobility$Mobility[match(substr(species.selected,1,6),sp.mobility$Abbrev)])
-  if(least.mobility==low.mobility){
-    reduce.PCA1.SegNo<-reachable.PCA.L[[match(remaining.seg[n],PCA1_SegNo)]]
-  }
-  if(least.mobility==medium.mobility){
-    reduce.PCA1.SegNo<-reachable.PCA.M[[match(remaining.seg[n],PCA1_SegNo)]]
-  }
-  if(least.mobility==high.mobility){
-    reduce.PCA1.SegNo<-reachable.PCA.H[[match(remaining.seg[n],PCA1_SegNo)]]
-  }
+  #species.selected<-colnames(species.distribution.df[,-1])[species.distribution.df[match(remaining.seg[n],species.distribution.df$SegNo),-1]==1]
+  #least.mobility<-min(sp.mobility$Mobility[match(substr(species.selected,1,6),sp.mobility$Abbrev)])
+  #if(least.mobility==low.mobility){
+  #  reduce.PCA1.SegNo<-reachable.PCA.L[[match(remaining.seg[n],PCA1_SegNo)]]
+  #}
+  #if(least.mobility==medium.mobility){
+  #  reduce.PCA1.SegNo<-reachable.PCA.M[[match(remaining.seg[n],PCA1_SegNo)]]
+  #}
+  #if(least.mobility==high.mobility){
+  #  reduce.PCA1.SegNo<-reachable.PCA.H[[match(remaining.seg[n],PCA1_SegNo)]]
+  #}
   
   # 2) segments not contributing to species feature
   # identify which species protection is still needed
@@ -119,7 +119,7 @@ for(i in 1:1000){
   # 3) update the selection pool formally
   #remaining.seg<-setdiff(remaining.seg,base::union(reduce.SP.SegNo,prot.species.df$SegNo))
   #remaining.seg<-setdiff(remaining.seg,prot.species.df$SegNo)
-  remaining.seg<-setdiff(remaining.seg,base::union(prot.species.df$SegNo,reduce.PCA1.SegNo))
+  #remaining.seg<-setdiff(remaining.seg,reduce.PCA1.SegNo)
   
   #------
   # 4. iteratively try a new segment to add to the current configuration
@@ -128,85 +128,132 @@ for(i in 1:1000){
   # 3) if improving (reducing), keep it in; if worsening (increasing), remove it.
   #------
   iteration.n<-0
-  while(iteration.n<=1000&sum(colSums(prot.species.df[,-1])>=cons.target)<25){
+  #while(iteration.n<=1000&sum(colSums(prot.species.df[,-1])>=cons.target)<25){
+  while(iteration.n<=1000){
+    
     # 1) randomly select a new segment
     n<-sample(1:length(remaining.seg),1)
     
-    # 2) check whether the new segment can improve/wosen the value of objective function
-    # check whether conservation target is met
-    prot.species<-species.distribution.df[match(remaining.seg[n],species.distribution.df$SegNo),]
-    prot.species.df.2<-rbind(prot.species.df,prot.species)
-    
-    # calculate the value of objective function
-    RDI<-candidate.df$RDI[match(prot.species.df.2$SegNo,candidate.df$SegNo)]
-    BC<-candidate.df$BC.nor[match(prot.species.df.2$SegNo,candidate.df$SegNo)]
-    feature<-cons.target-colSums(prot.species.df.2[,-1])
-    feature<-sapply(feature,FUN = function(x) {ifelse(x<0,0,x)})
-    
-    objective.func.2<-sum(RDI)+sum(para.a*(1-BC))+sum(para.b*feature)  # need to adjust para.b
-    
-    if(objective.func>=objective.func.2){
-      prot.species.df<-prot.species.df.2   # update current configuration
-      objective.func<-objective.func.2    # update the value of objective function
+    # check whether the selected segment is in the current configuration.
+    if(remaining.seg[n] %in% prot.species.df$SegNo){
+      prot.species.df.2<-prot.species.df[-match(remaining.seg[n],prot.species.df$SegNo),]
       
-      # update remaining pool
-      # 1) reachable segments from the retained
-      species.selected<-colnames(species.distribution.df[,-1])[species.distribution.df[match(remaining.seg[n],species.distribution.df$SegNo),-1]==1]
-      least.mobility<-min(sp.mobility$Mobility[match(substr(species.selected,1,6),sp.mobility$Abbrev)])
-      if(least.mobility==low.mobility){
-        reduce.PCA1.SegNo<-reachable.PCA.L[[match(remaining.seg[n],PCA1_SegNo)]]
+      RDI<-candidate.df$RDI[match(prot.species.df.2$SegNo,candidate.df$SegNo)]
+      BC<-candidate.df$BC.nor[match(prot.species.df.2$SegNo,candidate.df$SegNo)]
+      feature<-cons.target-colSums(prot.species.df.2[,-1])
+      feature<-sapply(feature,FUN = function(x) {ifelse(x<0,0,x)})
+      
+      objective.func.2<-sum(RDI)+sum(para.a*(1-BC))+sum(para.b*feature)  # need to adjust para.b
+      
+      if(objective.func>=objective.func.2){
+        prot.species.df<-prot.species.df.2   # update current configuration
+        objective.func<-objective.func.2    # update the value of objective function
+        
+        # update remaining seg
+        #species.selected<-colnames(species.distribution.df[,-1])[species.distribution.df[match(remaining.seg[n],species.distribution.df$SegNo),-1]==1]
+        #least.mobility<-min(sp.mobility$Mobility[match(substr(species.selected,1,6),sp.mobility$Abbrev)])
+        #if(least.mobility==low.mobility){
+        #  reduce.PCA1.SegNo<-reachable.PCA.L[[match(remaining.seg[n],PCA1_SegNo)]]
+        #}
+        #if(least.mobility==medium.mobility){
+        #  reduce.PCA1.SegNo<-reachable.PCA.M[[match(remaining.seg[n],PCA1_SegNo)]]
+        #}
+        #if(least.mobility==high.mobility){
+        #  reduce.PCA1.SegNo<-reachable.PCA.H[[match(remaining.seg[n],PCA1_SegNo)]]
+        #}    
+        #
+        #remaining.seg<-c(remaining.seg,reduce.PCA1.SegNo)
       }
-      if(least.mobility==medium.mobility){
-        reduce.PCA1.SegNo<-reachable.PCA.M[[match(remaining.seg[n],PCA1_SegNo)]]
+      else{
+        #cat(iteration.n,"\n")
+        iteration.n<-iteration.n+1
       }
-      if(least.mobility==high.mobility){
-        reduce.PCA1.SegNo<-reachable.PCA.H[[match(remaining.seg[n],PCA1_SegNo)]]
-      }
-      
-      # 2) segments not contributing to species feature
-      # identify which species protection is still needed
-      #deficit.species<-colnames(prot.species.df[,-1])[colSums(prot.species.df[,-1])<cons.target]
-      #col.deficit<-match(deficit.species,colnames(species.distribution.df[,-1]))
-      #if(length(col.deficit)>1){
-      #  update.SP.SegNo<-species.distribution.df$SegNo[rowSums(species.distribution.df[,(col.deficit+1)]==1)>0]
-      #  reduce.SP.SegNo<-setdiff(species.distribution.df$SegNo,update.SP.SegNo)
-      #}
-      #if(length(col.deficit)==1){
-      #  update.SP.SegNo<-species.distribution.df$SegNo[species.distribution.df[,(col.deficit+1)]==1]
-      #  reduce.SP.SegNo<-setdiff(species.distribution.df$SegNo,update.SP.SegNo)
-      #}
-      
-      # 3) update the selection pool formally
-      #remaining.seg<-setdiff(remaining.seg,union(reduce.SP.SegNo,reduce.PCA1.SegNo))
-      #remaining.seg<-setdiff(remaining.seg,base::union(reduce.SP.SegNo,prot.species.df$SegNo))
-      #remaining.seg<-setdiff(remaining.seg,prot.species.df$SegNo)
-      remaining.seg<-setdiff(remaining.seg,base::union(prot.species.df$SegNo,reduce.PCA1.SegNo))
-      
-      
-      if(length(remaining.seg)==0){
-        zero.remaining.pool<-zero.remaining.pool+1
-        break
-      }
-      iteration.n<-0
     }
     else{
-      #cat(iteration.n,"\n")
-      iteration.n<-iteration.n+1
+      prot.species<-species.distribution.df[match(remaining.seg[n],species.distribution.df$SegNo),]
+      prot.species.df.2<-rbind(prot.species.df,prot.species)
+      
+      # 2) check whether the new segment can improve/wosen the value of objective function
+      # calculate the value of objective function
+      RDI<-candidate.df$RDI[match(prot.species.df.2$SegNo,candidate.df$SegNo)]
+      BC<-candidate.df$BC.nor[match(prot.species.df.2$SegNo,candidate.df$SegNo)]
+      feature<-cons.target-colSums(prot.species.df.2[,-1])
+      feature<-sapply(feature,FUN = function(x) {ifelse(x<0,0,x)})
+      
+      objective.func.2<-sum(RDI)+sum(para.a*(1-BC))+sum(para.b*feature)  # need to adjust para.b
+      
+      if(objective.func>=objective.func.2){
+        prot.species.df<-prot.species.df.2   # update current configuration
+        objective.func<-objective.func.2    # update the value of objective function
+        
+        # update remaining pool
+        # 1) reachable segments from the retained
+        #species.selected<-colnames(species.distribution.df[,-1])[species.distribution.df[match(remaining.seg[n],species.distribution.df$SegNo),-1]==1]
+        #least.mobility<-min(sp.mobility$Mobility[match(substr(species.selected,1,6),sp.mobility$Abbrev)])
+        #if(least.mobility==low.mobility){
+        #  reduce.PCA1.SegNo<-reachable.PCA.L[[match(remaining.seg[n],PCA1_SegNo)]]
+        #}
+        #if(least.mobility==medium.mobility){
+        #  reduce.PCA1.SegNo<-reachable.PCA.M[[match(remaining.seg[n],PCA1_SegNo)]]
+        #}
+        #if(least.mobility==high.mobility){
+        #  reduce.PCA1.SegNo<-reachable.PCA.H[[match(remaining.seg[n],PCA1_SegNo)]]
+        #}
+        
+        # 2) segments not contributing to species feature
+        # identify which species protection is still needed
+        #deficit.species<-colnames(prot.species.df[,-1])[colSums(prot.species.df[,-1])<cons.target]
+        #col.deficit<-match(deficit.species,colnames(species.distribution.df[,-1]))
+        #if(length(col.deficit)>1){
+        #  update.SP.SegNo<-species.distribution.df$SegNo[rowSums(species.distribution.df[,(col.deficit+1)]==1)>0]
+        #  reduce.SP.SegNo<-setdiff(species.distribution.df$SegNo,update.SP.SegNo)
+        #}
+        #if(length(col.deficit)==1){
+        #  update.SP.SegNo<-species.distribution.df$SegNo[species.distribution.df[,(col.deficit+1)]==1]
+        #  reduce.SP.SegNo<-setdiff(species.distribution.df$SegNo,update.SP.SegNo)
+        #}
+        
+        # 3) update the selection pool formally
+        #remaining.seg<-setdiff(remaining.seg,union(reduce.SP.SegNo,reduce.PCA1.SegNo))
+        #remaining.seg<-setdiff(remaining.seg,base::union(reduce.SP.SegNo,prot.species.df$SegNo))
+        #remaining.seg<-setdiff(remaining.seg,prot.species.df$SegNo)
+        #remaining.seg<-setdiff(remaining.seg,reduce.PCA1.SegNo)
+        
+        
+        if(length(remaining.seg)==0){
+          zero.remaining.pool<-zero.remaining.pool+1
+          break
+        }
+        iteration.n<-0
+      }
+      else{
+        #cat(iteration.n,"\n")
+        iteration.n<-iteration.n+1
+      }
     }
-    
+    cat(nrow(prot.species.df),"\n",file = "dynamics of selection_b015.txt",append = TRUE)
   }
   n.seg[i]<-nrow(prot.species.df)
   solution.lst[[i]]<-prot.species.df$SegNo
   rep.sp[i]<-sum(colSums(prot.species.df[,-1])>=cons.target)
   obj.func[i]<-objective.func
   
-  cat(i,"\n")
+  cat("new loop ",i,"\n")
 }
 
 zero.remaining.pool
 frequency.seg<-data.frame(table(unlist(solution.lst)))
-summary(frequency.seg$Freq/1000)
+summary(frequency.seg$Freq/2)
 summary(n.seg)
+
+# sp representation of the best solution
+plot(colSums(prot.species.df[,c(2:26)])/n.sp)
+
+
+# sp rep for randomly selected segments in the same number as that systematically selected
+random.seg<-PCA1_SegNo[runif(357,min = 1,max=length(PCA1_SegNo))]
+plot(colSums(species.distribution.df[match(random.seg,species.distribution.df$SegNo),-1])/n.sp)
+
 
 saveRDS(solution.lst,file = "Data/R data/PCA3 solution tgt 15")
 saveRDS(rep.sp,file = "Data/R data/PCA3 SpRep tgt 15")
