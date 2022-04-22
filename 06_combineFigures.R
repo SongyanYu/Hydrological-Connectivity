d567f01599da23860aa880fb55aeba49fe7191fa
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2022-04-22 18:06:40 +1000

    update crop extent

--- 06_combineFigures.R
+++ 06_combineFigures.R
@@ -11,20 +11,23 @@ library(magick)
 library(magrittr)  # to activate the pipe syntax
 
 bc <- image_read(path = "../../Figures/Fig02_BC.jpg") %>%
-  image_crop("7000x6000+2200+500") %>%
-  image_annotate("(a)", size = 300, location = "+6000+100")
+  image_crop("3300x2900+1200+200") %>%
+  image_annotate("(a)", size = 200, location = "+2900+100")
+
+#image_browse(bc)
 
 sprep <- image_read(path = "../../Figures/Fig02_SpRichness.jpg")%>%
-  image_crop("7000x6000+2200+500") %>%
-  image_annotate("(b)", size = 300, location = "+6000+100")
+  image_crop("3300x2900+1200+200") %>%
+  image_annotate("(b)", size = 200, location = "+2900+100")
 
 years <- image_read(path = "../../Figures/Fig02_#years.jpg") %>%
-  image_crop("7000x6000+2200+500") %>%
-  image_annotate("(c)", size = 300, location = "+6000+100")
+  image_crop("3300x2900+1200+200") %>%
+  image_annotate("(c)", size = 200, location = "+2900+100")
 
 rdi <- image_read(path = "../../Figures/Fig02_RDI.jpg") %>%
-  image_crop("7000x6000+2200+500") %>%
-  image_annotate("(d)", size = 300, location = "+6000+100")
+  image_crop("3300x2900+1200+200") %>%
+  image_annotate("(d)", size = 200, location = "+2900+100")
+
 
 gc()
 
@@ -40,35 +43,37 @@ gc()
 
 priority.015 <- 
   image_read(path = "../../Figures/Fig03_PriorityNetwork_Target15.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(a)", size = 300, location = "+6000+100")
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(a)", size = 200, location = "+2200+100")
+
+#image_browse(priority.015)
 
 priority.025 <- 
   image_read(path = "../../Figures/Fig03_PriorityNetwork_Target25.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(c)", size = 300, location = "+6000+100")
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(c)", size = 200, location = "+2200+100")
 
 priority.035 <- 
   image_read(path = "../../Figures/Fig03_PriorityNetwork_Target35.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(e)", size = 300, location = "+6000+100")
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(e)", size = 200, location = "+2200+100")
 
 gc()
 
 selecFreq.015 <- 
-  image_read(path = "../../Figures/Fig04_SelecFreq_Target15.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(b)", size = 300, location = "+6000+100")
+  image_read(path = "../../Figures/Fig03_SelecFreq_Target15.jpg") %>%
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(b)", size = 200, location = "+2200+100")
 
 selecFreq.025 <- 
-  image_read(path = "../../Figures/Fig04_SelecFreq_Target25.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(d)", size = 300, location = "+6000+100")
+  image_read(path = "../../Figures/Fig03_SelecFreq_Target25.jpg") %>%
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(d)", size = 200, location = "+2200+100")
 
 selecFreq.035 <- 
-  image_read(path = "../../Figures/Fig04_SelecFreq_Target35.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(f)", size = 300, location = "+6000+100")
+  image_read(path = "../../Figures/Fig03_SelecFreq_Target35.jpg") %>%
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(f)", size = 200, location = "+2200+100")
 
 target_015 <- image_append(c(priority.015, selecFreq.015))
 target_025 <- image_append(c(priority.025, selecFreq.025))
@@ -83,35 +88,35 @@ image_write(image_priority_all, path = '../../Figures/Fig03_ALL_r.jpg', format =
 
 priority.015 <- 
   image_read(path = "../../Figures/Fig03_PriorityNetwork_Target15_NoLockedIn.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(a)", size = 300, location = "+6000+100")
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(a)", size = 200, location = "+2200+100")
 
 priority.025 <- 
   image_read(path = "../../Figures/Fig03_PriorityNetwork_Target25_NoLockedIn.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(c)", size = 300, location = "+6000+100")
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(c)", size = 200, location = "+2200+100")
 
 priority.035 <- 
   image_read(path = "../../Figures/Fig03_PriorityNetwork_Target35_NoLockedIn.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(e)", size = 300, location = "+6000+100")
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(e)", size = 200, location = "+2200+100")
 
 gc()
 
 selecFreq.015 <- 
-  image_read(path = "../../Figures/Fig04_SelecFreq_Target15_NoLockedIn.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(b)", size = 300, location = "+6000+100")
+  image_read(path = "../../Figures/Fig03_SelecFreq_Target15_NoLockedIn.jpg") %>%
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(b)", size = 200, location = "+2200+100")
 
 selecFreq.025 <- 
-  image_read(path = "../../Figures/Fig04_SelecFreq_Target25_NoLockedIn.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(d)", size = 300, location = "+6000+100")
+  image_read(path = "../../Figures/Fig03_SelecFreq_Target25_NoLockedIn.jpg") %>%
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(d)", size = 200, location = "+2200+100")
 
 selecFreq.035 <- 
-  image_read(path = "../../Figures/Fig04_SelecFreq_Target35_NoLockedIn.jpg") %>%
-  image_crop("6800x5800+2200+500") %>%
-  image_annotate("(f)", size = 300, location = "+6000+100")
+  image_read(path = "../../Figures/Fig03_SelecFreq_Target35_NoLockedIn.jpg") %>%
+  image_crop("2500x2200+800+200") %>%
+  image_annotate("(f)", size = 200, location = "+2200+100")
 
 target_015 <- image_append(c(priority.015, selecFreq.015))
 target_025 <- image_append(c(priority.025, selecFreq.025))
